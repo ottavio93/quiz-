960eb59ce278bdf9c932c1e554db219908d46365
Ottavio93 <lopiccoloottavio@gmail.com>
2021-05-10 09:41:21 +0200

    gggg

--- src/app/board-moderator/board-moderator.component.ts
+++ src/app/board-moderator/board-moderator.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EnableUserRequest } from '../dto/EnableUserRequest';
+import { AuthService } from '../_services/auth.service';
+import { TokenStorageService } from '../_services/token-storage.service';
 import { UserService } from '../_services/user.service';
 
 @Component({
@@ -8,10 +11,18 @@ import { UserService } from '../_services/user.service';
 })
 export class BoardModeratorComponent implements OnInit {
   content: string;
-
-  constructor(private userService: UserService) {}
+  currentUser: any;
+  alluser: [];
+ 
+  constructor(private userService: UserService,  
+     private authService :AuthService, private token: TokenStorageService) {}
 
   ngOnInit(): void {
+    this.currentUser = this.token.getUser();
+    this.userService.getAllUser().subscribe((data) => {
+      this.alluser = data;
+      console.log(this.alluser);
+    });
     this.userService.getModeratorBoard().subscribe(
       (data) => {
         this.content = data;
@@ -21,4 +32,25 @@ export class BoardModeratorComponent implements OnInit {
       }
     );
   }
-}
+  disabledUser(username:string) {
+  
+    let model2=new EnableUserRequest(username);
+          this.authService.disableUser(model2).subscribe((data)=>{
+            console.log(username);
+            location.reload();
+         
+          });
+        }
+        enabledUser(username:string) {
+  
+          let model2=new EnableUserRequest(username);
+                this.authService.enableUser(model2).subscribe((data)=>{
+                  console.log(username);
+                  location.reload();
+               
+                });
+              }
+        
+        
+        
+        }
